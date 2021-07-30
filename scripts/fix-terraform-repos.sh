bbdb35aad7285da37ab5454cf2f8dc0a8cd84d99
Chris Gilmer <chris.gilmer@gmail.com>
2021-07-30 10:18:25 -0700

    Manage more repos and include terratest

--- scripts/fix-terraform-repos.sh
+++ scripts/fix-terraform-repos.sh
@@ -48,37 +48,65 @@ check() {
 
     basedir=$(realpath "${DIR}"/../../)
 
+    echo
+    echo "===================="
+    echo "Stash changes and pull latest code"
+    pwd
+    git stash
+    git checkout main || git checkout master
+    git pull
+
+    echo
+    echo "===================="
+    echo "Make directories"
+    # Make directories
+    mkdir -p .circleci/
+    mkdir -p bin/
+    mkdir -p examples/simple/
+    mkdir -p pkg/
+    mkdir -p scripts/
+    mkdir -p test/
+
+    echo
+    echo "===================="
+    echo "Update to latest"
     # Files we want to be exactly the same as in the template repo
     diffcheck "${basedir}" "${repo}" .circleci/config.yml
-    diffcheck "${basedir}" "${repo}" scripts/format-files
+    diffcheck "${basedir}" "${repo}" scripts/format-terraform
+    diffcheck "${basedir}" "${repo}" scripts/lint-go
+    diffcheck "${basedir}" "${repo}" scripts/terratest
     diffcheck "${basedir}" "${repo}" scripts/update-docs
+    diffcheck "${basedir}" "${repo}" .editorconfig
+    diffcheck "${basedir}" "${repo}" .envrc
     diffcheck "${basedir}" "${repo}" .gitignore
     diffcheck "${basedir}" "${repo}" .markdownlintrc
     diffcheck "${basedir}" "${repo}" .pre-commit-config.yaml
     diffcheck "${basedir}" "${repo}" LICENSE
-    diffcheck "${basedir}" "${repo}" versions.tf
+    diffcheck "${basedir}" "${repo}" INTENT.md
+    diffcheck "${basedir}" "${repo}" Makefile
     pushd "${basedir}/${repo}" >> /dev/null || exit 1
+
     echo
     echo "===================="
-    pwd
+    echo "Remove old files"
+    rm -f scripts/format-files
+    rm -f .terraform-version
+
+    echo "===================="
+    echo "Repo status"
     echo
-    git stash
-    git checkout main || git checkout master
-    git pull
-    git stash apply
     git status
-    git checkout -b "fix_repo_${DATE}"
-    git add .
-    git commit -am"Update repo to match template repo"
-    gpthis
-    gpr
+
     popd >> /dev/null || exit 1
 }
 
 check terraform-aws-acm-certificate
 check terraform-aws-alb-metric-alarms
 check terraform-aws-api-gateway-cloudwatch-role
+check terraform-aws-api-gateway-cors-lambda-proxy
 check terraform-aws-api-gateway-waf
+check terraform-aws-athena-iam-policy
+check terraform-aws-athena-workgroup
 check terraform-aws-cloudfront-bucket
 check terraform-aws-cloudfront-waf
 check terraform-aws-cloudwatch-kms-key
@@ -88,9 +116,17 @@ check terraform-aws-ds-directory
 check terraform-aws-dynamodb-kms-key
 check terraform-aws-ebs-kms-key
 check terraform-aws-ec2-instance-role
+check terraform-aws-ecr-iam-policy
 check terraform-aws-ecr-kms-key
+check terraform-aws-ecr-repo
+check terraform-aws-ecs-cluster
 check terraform-aws-ecs-metric-alarms
+check terraform-aws-ecs-scheduled-task
+check terraform-aws-ecs-task-execution-role
+check terraform-aws-ecs-task-role
 check terraform-aws-elasticsearch-domain
+check terraform-aws-glue-data-catalog-encryption
+check terraform-aws-glue-iam-policy
 check terraform-aws-glue-iam-role
 check terraform-aws-glue-kms-key
 check terraform-aws-guardduty
@@ -104,12 +140,21 @@ check terraform-aws-lambda-edge-function
 check terraform-aws-lambda-function
 check terraform-aws-lambda-kms-key
 check terraform-aws-lambda-metric-alarms
+check terraform-aws-no-delete-policy
+check terraform-aws-notify-slack-kms
 check terraform-aws-rest-endpoint
+check terraform-aws-s3-access-logs
+check terraform-aws-s3-data-to-parquet
+check terraform-aws-s3-iam-policy
 check terraform-aws-s3-kms-key
+check terraform-aws-sagemaker-iam-role
+check terraform-aws-sagemaker-kms-key
 check terraform-aws-self-manage-credentials
 check terraform-aws-ses-domain
 check terraform-aws-sns-kms-key
 check terraform-aws-sns-topic
+check terraform-aws-sqs-kms-key
+check terraform-aws-sqs-queue
 check terraform-aws-ssm-kms-key
 check terraform-aws-sso-roles
 check terraform-aws-user-management
